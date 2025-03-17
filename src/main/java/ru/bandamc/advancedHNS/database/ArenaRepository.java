1e488f2e4c15a1848fcbc4c0a81cd4066761970d
Twelvee <redapple54r@gmail.com>
2025-03-18 04:09:30 +0700

    feat: Update arena from entity

--- src/main/java/ru/bandamc/advancedHNS/database/ArenaRepository.java
+++ src/main/java/ru/bandamc/advancedHNS/database/ArenaRepository.java
@@ -56,6 +56,18 @@ public class ArenaRepository {
     }
 
     public void updateArena(Arena arena) throws SQLException {
-
+        PreparedStatement sql = plugin.getConnection().prepareStatement("UPDATE `hns_arenas` SET `world`=?, `max_seekers`=?, `max_hiders`=?, `status`=?, `pos1`=?, `pos2`=?, `spec_pos`=?, `lobby_pos`=?, `seekers_spawn_point`=?, `hiders_spawn_point`=? WHERE `name`=?");
+        sql.setString(1, arena.getWorld());
+        sql.setInt(2, arena.getMaxSeekers());
+        sql.setInt(3, arena.getMaxHiders());
+        sql.setInt(4, 1); // 0 - editing, 1 - available, 2 - waiting for players, 3 - playing, 4 - cleaning everything up
+        sql.setString(5, arena.getPos1().getWorld().getName()+"|"+arena.getPos1().getX()+"|"+arena.getPos1().getY()+"|"+arena.getPos1().getZ());
+        sql.setString(6, arena.getPos2().getWorld().getName()+"|"+arena.getPos2().getX()+"|"+arena.getPos2().getY()+"|"+arena.getPos2().getZ());
+        sql.setString(7, arena.getSpecPoint().getWorld().getName()+"|"+arena.getSpecPoint().getX()+"|"+arena.getSpecPoint().getY()+"|"+arena.getSpecPoint().getZ());
+        sql.setString(8, arena.getLobbyPos().getWorld().getName()+"|"+arena.getLobbyPos().getX()+"|"+arena.getLobbyPos().getY()+"|"+arena.getLobbyPos().getZ());
+        sql.setString(9, arena.getSeekersSpawnPos().getWorld().getName()+"|"+arena.getSeekersSpawnPos().getX()+"|"+arena.getSeekersSpawnPos().getY()+"|"+arena.getSeekersSpawnPos().getZ());
+        sql.setString(10, arena.getHidersSpawnPos().getWorld().getName()+"|"+arena.getHidersSpawnPos().getX()+"|"+arena.getHidersSpawnPos().getY()+"|"+arena.getHidersSpawnPos().getZ());
+        sql.setString(11, arena.getName());
+        sql.execute();
     }
 }
