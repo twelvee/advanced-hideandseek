06c66d2c3637ba72bb213d8d9191bc02337b060e
Twelvee <redapple54r@gmail.com>
2025-03-18 00:56:32 +0700

    feat: commands autocomplete

--- src/main/java/ru/bandamc/advancedHNS/command/CommandExecutor.java
+++ src/main/java/ru/bandamc/advancedHNS/command/CommandExecutor.java
@@ -1,11 +1,19 @@
 package ru.bandamc.advancedHNS.command;
 
+import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.bandamc.advancedHNS.AdvancedHNS;
 
-public class CommandExecutor implements org.bukkit.command.CommandExecutor {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CommandExecutor implements org.bukkit.command.CommandExecutor, TabCompleter {
     private final AboutCommandHandler aboutCommandHandler;
     private final HelpCommandHandler helpCommandHandler;
     private final ChangeWorldCommandHandler worldTpCommandHandler;
@@ -32,5 +40,31 @@ public class CommandExecutor implements org.bukkit.command.CommandExecutor {
         }
         sender.sendMessage(AdvancedHNS.HNS_CHAT_PREFIX + " Unknown command.");
         return false;
+    }
+
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length == 1 && args[0].isEmpty()) {
+            return Arrays.asList("help", "admin");
+        }
+        if (args.length == 2 && args[0].equalsIgnoreCase("help")) {
+            return List.of();
+        }
+        if (args.length == 2 && args[0].equalsIgnoreCase("admin")) {
+            return Arrays.asList("world", "arena");
+        }
+        if (args.length == 3 && args[0].equalsIgnoreCase("admin") && args[1].equalsIgnoreCase("world")) {
+            return List.of("tp");
+        }
+        if (args.length == 4 && args[0].equalsIgnoreCase("admin") && args[1].equalsIgnoreCase("world") && args[2].equalsIgnoreCase("tp")) {
+            ArrayList<String> worlds = new ArrayList<>();
+            for (World world : Bukkit.getWorlds()) {
+                worlds.add(world.getName());
+            }
+            return worlds;
+        }
+
+        return List.of();
     }
 }
