492b1ef55363788dc78d84c6954b06d0f7e2899c
Twelvee <redapple54r@gmail.com>
2025-03-19 01:08:49 +0700

    feat: teleport player on spawn && fix: player left event call

--- src/main/java/ru/bandamc/advancedHNS/events/OnPlayerJoinServerEvent.java
+++ src/main/java/ru/bandamc/advancedHNS/events/OnPlayerJoinServerEvent.java
@@ -4,10 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.bandamc.advancedHNS.AdvancedHNS;
 
 public class OnPlayerJoinServerEvent implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-
+        AdvancedHNS plugin = JavaPlugin.getPlugin(AdvancedHNS.class);
+        // teleport players to spawn on every join.
+        if (plugin.getConfig().getBoolean("teleport_to_spawn_on_join"))
+            event.getPlayer().teleport(Bukkit.getWorld(plugin.getConfig().getString("spawn_world", "world")).getSpawnLocation());
     }
 }
--- src/main/java/ru/bandamc/advancedHNS/events/OnPlayerLeftServerEvent.java
+++ src/main/java/ru/bandamc/advancedHNS/events/OnPlayerLeftServerEvent.java
@@ -1,16 +1,30 @@
 package ru.bandamc.advancedHNS.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.bandamc.advancedHNS.AdvancedHNS;
+import ru.bandamc.advancedHNS.api.events.ArenaLeaveEvent;
+import ru.bandamc.advancedHNS.entities.Arena;
 
 public class OnPlayerLeftServerEvent implements Listener {
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
         AdvancedHNS plugin = JavaPlugin.getPlugin(AdvancedHNS.class);
         plugin.arenaEdits.remove(event.getPlayer());
+        Arena arena = plugin.playerArena.get(event.getPlayer());
+        if (arena != null) {
+            if (plugin.arenaPlayers.get(arena) != null) {
+                plugin.arenaPlayers.get(arena).remove(event.getPlayer());
+            }
+            plugin.playerArena.remove(event.getPlayer());
+
+            // call arena leave event if player was on arena.
+            ArenaLeaveEvent leaveEvent = new ArenaLeaveEvent(event.getPlayer(), arena);
+            Bukkit.getPluginManager().callEvent(leaveEvent);
+        }
     }
 }
